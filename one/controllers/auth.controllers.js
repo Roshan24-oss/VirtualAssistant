671f545cbb56a4fcde4ab30bf38accb861877656
RoshanKhadayat <roshankhadayat72052@gmail.com>
2025-08-16 15:30:23 +0545

    Update auth.controllers.js

--- one/controllers/auth.controllers.js
+++ one/controllers/auth.controllers.js
@@ -32,7 +32,7 @@ export const signUp = async (req, res) => {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       sameSite: "strict",
-      secure: process.env.NODE_ENV === "production", // secure only in production
+      secure:true
     });
 
     return res.status(201).json({ message: "User created successfully" });
@@ -62,8 +62,8 @@ export const Login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: process.env.NODE_ENV === "production",
+      sameSite: "None",
+      secure:true
     });
 
     // Only return safe user info
